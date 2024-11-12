71858b3b289350a82a9f55d74e565a83ae5398d7
etericeferova <147832143+etericeferova@users.noreply.github.com>
2024-11-12 22:09:44 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log("Hello");
+// 1
+// let name = prompt("Enter your name: ");
+// alert("Hello, " + name);
+
+// 2
+// const currentYear = 2024;
+// let birthYear = prompt("Enter your Birth year: ");
+// alert(currentYear - birthYear);
+
+// 3
+// let side = prompt("Enter the side of square: ");
+// let perimeter = 4 * side;
+// alert("Perimeter : " + perimeter);
+
+// 4
+// let radius = prompt("Enter the radius: ");
+// const pi = 3.14;
+// let area = pi * radius ** 2;
+// alert("Area : " + area);
+
+// 5
+// let distance = prompt("Enter the distance between cities: ");
+// let time = prompt("Enter the time: ");
+// let speed = distance / time;
+// alert("Speed: " + speed);
+
+// 6
+// const EURO = 0.94;
+// let moneyInDollars = prompt("Enter the money value to convert in dollars: ");
+// let converter = moneyInDollars * EURO;
+// alert("Converted money: " + converter);
+
+// 7
+// let flashSize = prompt("Enter the size of flash: ");
+// let sizeConverter = flashSize * 1024;
+// const fileSize = 820;
+// let numberOfFiles = Math.floor(sizeConverter / fileSize);
+// alert("Number of files: " + numberOfFiles);
+
+// 8
+// let moneyHave = prompt("Enter money you have: ");
+// let chocolateCost = prompt("Enter the price of chocolate: ");
+// let chocolateBuy = Math.floor(moneyHave / chocolateCost);
+// let moneyRest = moneyHave % chocolateCost;
+// alert("Can buy: " + chocolateBuy);
+// alert("Money rest: " + moneyRest);
+
+// 9
+// let number = prompt("Enter the number: ");
+// let unit = number % 10;
+// let tens = Math.floor((number / 10) % 10);
+// let hundred = Math.floor(number / 100);
+// let newNumber = unit * 100 + tens * 10 + hundred;
+// alert("New number: " + newNumber);
+
+//10
+// let number = prompt("Enter the number: ");
+// if (number % 2 == 0) {
+//   alert("Number is even.");
+// }
+// if (number % 2 != 0) {
+//   alert("Number is odd.");
+// }
