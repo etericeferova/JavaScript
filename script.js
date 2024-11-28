45da8dd71c26855cd1782cb3b65edd0f2e13b293
etericeferova <147832143+etericeferova@users.noreply.github.com>
2024-11-28 23:57:24 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+document.addEventListener("DOMContentLoaded", () => {
+  const inputField = document.getElementById("username");
+
+  inputField.addEventListener("input", (event) => {
+    const currentValue = event.target.value;
+    const filteredChars = [];
+
+    for (let char of currentValue) {
+      if (!char.match(/\d/)) {
+        filteredChars.push(char);
+      }
+    }
+    event.target.value = filteredChars.join("");
+  });
+});
