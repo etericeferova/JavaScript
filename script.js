e637d9e16666c296997fc071e03793a7261b2a82
etericeferova <147832143+etericeferova@users.noreply.github.com>
2024-11-28 23:55:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const modalOverlay = document.getElementById("modalOverlay");
+
+openModalBtn.addEventListener("click", () => {
+  modalOverlay.style.display = "flex";
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modalOverlay.style.display = "none";
+});
